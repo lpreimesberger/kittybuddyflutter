804210c160bf1c7997c2e0e39b8d124337a6bd61
lpreimesberger <lpreimesberger@gmail.com>
2023-02-20 18:30:04 -0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,199 @@
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:kittybuddyflutter/phrases.dart';
+import 'package:text_to_speech/text_to_speech.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final String defaultLanguage = 'en-GB';
+  final _random = Random();
+  TextToSpeech tts = TextToSpeech();
+
+  String text = '';
+  double volume = .5; // Range: 0-1
+  double rate = 1.0; // Range: 0-2
+  double pitch = .8; // Range: 0-2
+
+  String? language;
+  String? languageCode;
+  List<String> languages = <String>[];
+  List<String> languageCodes = <String>[];
+  String? voice;
+
+  TextEditingController textEditingController = TextEditingController(text: "i am a cat");
+
+  @override
+  void initState() {
+    super.initState();
+    textEditingController.text = text;
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      initLanguages();
+    });
+  }
+
+  Future<void> initLanguages() async {
+    languageCodes = await tts.getLanguages();
+    final List<String>? displayLanguages = await tts.getDisplayLanguages();
+    if (displayLanguages == null) {
+      return;
+    }
+
+    languages.clear();
+    for (final dynamic lang in displayLanguages) {
+      languages.add(lang as String);
+    }
+
+    final String? defaultLangCode = await tts.getDefaultLanguage();
+    if (defaultLangCode != null && languageCodes.contains(defaultLangCode)) {
+      languageCode = defaultLangCode;
+    } else {
+      languageCode = defaultLanguage;
+    }
+    language = await tts.getDisplayLanguageByCode(languageCode!);
+
+    /// get voice
+    voice = await getVoiceByLang(languageCode!);
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  Future<String?> getVoiceByLang(String lang) async {
+    final List<String>? voices = await tts.getVoiceByLang(languageCode!);
+    if (voices != null && voices.isNotEmpty) {
+      return voices.first;
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Kitsune Meat World Extension 2023.1'),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Image.asset('assets/kitsune.png'),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * .9,
+                        child: Text(
+                          text,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.only(right: 10),
+                          child: ElevatedButton(
+                            child: const Text('Say Something'),
+                            onPressed: () {
+                              setState(() {
+                                text = phrases[_random.nextInt(phrases.length)];
+                              });
+
+                              sayThis(text);
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+
+  }
+
+  bool get supportPause => defaultTargetPlatform != TargetPlatform.android;
+
+  bool get supportResume => defaultTargetPlatform != TargetPlatform.android;
+
+  void speak() {
+    tts.setVolume(volume);
+    tts.setRate(rate);
+    if (languageCode != null) {
+      tts.setLanguage(languageCode!);
+    }
+    tts.setPitch(pitch);
+    tts.speak(text);
+  }
+  void sayThis(String thisString ) {
+    tts.setVolume(volume);
+    tts.setRate(rate);
+    if (languageCode != null) {
+      tts.setLanguage(languageCode!);
+    }
+    tts.setPitch(pitch);
+    tts.speak(thisString);
+  }
+}
