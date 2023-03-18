c305580cf739cdb8a1d3ba309362c2128d6021ae
lpreimesberger <lpreimesberger@gmail.com>
2023-03-18 16:16:34 -0500

    - mouth works now

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,61 @@
 import 'dart:async';
-import 'dart:io';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'dart:math';
 import 'dart:ui';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:kittybuddyflutter/phrases.dart';
-import 'package:text_to_speech/text_to_speech.dart';
 
+
+var hoomans = 0;
+var hoomansv2 = 0;
 var globalWidth = 300.0;
 var globalHeight = 300.0;
 var mouth = 5.0;
 
+
+class Hooman {
+  final String eventtype;
+  final String hooman_id;
+  final String hooman_name;
+  final String hooman_likes;
+
+  Hooman(this.eventtype, this.hooman_id, this.hooman_name, this.hooman_likes) {
+
+  }
+
+  Hooman.fromJson(Map<String, dynamic> json)
+      : eventtype = json['eventtype'],
+  hooman_name = json['hooman_name'],
+        hooman_likes = json['hooman_likes'],
+        hooman_id = json['hooman_id'];
+
+  Map<String, dynamic> toJson() => {
+    'eventtype': eventtype,
+    'hooman_id': hooman_id,
+    'hooman_name': hooman_name,
+    'hooman_likes': hooman_likes,
+  };
+}
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key}){
+
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'MeatSpace',
       theme: ThemeData(
+        fontFamily: 'Robot',
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -43,7 +74,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final String defaultLanguage = 'en-GB';
   final _random = Random();
-  TextToSpeech tts = TextToSpeech();
   String text = '';
   double volume = .5; // Range: 0-1
   double rate = .9; // Range: 0-2
@@ -61,53 +91,57 @@ class _MyHomePageState extends State<MyHomePage> {
     textEditingController.text = text;
     WidgetsBinding.instance?.addPostFrameCallback((_) {
       initLanguages();
+      startListener();
     });
   }
 
   Future<void> initLanguages() async {
-    languageCodes = await tts.getLanguages();
-    final List<String>? displayLanguages = await tts.getDisplayLanguages();
-    if (displayLanguages == null) {
-      return;
-    }
-
-    languages.clear();
-    for (final dynamic lang in displayLanguages) {
-      languages.add(lang as String);
-    }
-
-    final String? defaultLangCode = await tts.getDefaultLanguage();
-    if (defaultLangCode != null && languageCodes.contains(defaultLangCode)) {
-      languageCode = defaultLangCode;
-    } else {
-      languageCode = defaultLanguage;
-    }
-    language = await tts.getDisplayLanguageByCode(languageCode!);
-
-    /// get voice
-    voice = await getVoiceByLang(languageCode!);
-
-    if (mounted) {
-      setState(() {});
-    }
   }
 
   Future<String?> getVoiceByLang(String lang) async {
-    final List<String>? voices = await tts.getVoiceByLang(languageCode!);
-
-    if (voices != null && voices.isNotEmpty) {
-      if( voices.contains('en-GB') ){
-          return 'en-GB';
-      }
-      return voices.first;
-    }
-    return null;
   }
+
+  startListener() async {
+    while(true){
+      var url = Uri.http('localhost:8080', 'events');
+      try {
+        var response = await http.read(url);
+        if(response.isNotEmpty) {
+          // python, bah
+          response = response.replaceFirst("b'", "").replaceFirst("'", "");
+          print('Response status: ${response}');
+
+          Map<String, dynamic> user = jsonDecode(response);
+          var hooman = Hooman.fromJson(user);
+          // {"eventtype":"hooman","hooman_id":"", "hooman_name":"", "hooman_likes":""}
+          if(hooman.eventtype == "hooman"){
+            setState(() {
+              hoomans = hoomans + 1;
+              text = phrases[_random.nextInt(phrases.length)];
+            });
+            sayThis(text);
+          }
+          if(hooman.eventtype == "tap"){
+            setState(() {
+              text = greet[_random.nextInt(greet.length)] + " Hooman ${hooman.hooman_name}." + vip[_random.nextInt(vip.length)];
+            });
+            sayThis(text);
+          }
+        }
+      } catch(e){
+        print(e);
+      }
+      print('listener zzz');
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black38,
+
         appBar: AppBar(
           title: const Text('Kitsune Meat World Extension 2023.1'),
         ),
@@ -115,22 +149,30 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Padding(
             padding: const EdgeInsets.all(20.0),
             child: Center(
+
               child: Column(
                 children: <Widget>[
                   Stack(
                     children: <Widget>[
-                      Image.asset('assets/kitsune.png'),
+                      Image.asset('assets/kitsune.png', height: 600),
                       CustomPaint(
                         painter: OpenPainter(),
                       ),
+                      Column(children: [
+                        Text("Hoomans : ${hoomans}"),
+                        Text("Carriers: ${hoomansv2}"),
+                        Text("X       : ${globalWidth}"),
+                        Text("Y       : ${globalHeight}"),
+                      ],)
                     ],
                   ),
 
                   const SizedBox(
                     height: 20,
                   ),
-                  Row(
-                    children: <Widget>[
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
 
                       SizedBox(
                         width: MediaQuery.of(context).size.width * .9,
@@ -151,9 +193,17 @@ class _MyHomePageState extends State<MyHomePage> {
                         child: Container(
                           padding: const EdgeInsets.only(right: 10),
                           child: ElevatedButton(
-                            child: const Text('Say Something'),
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.grey,
+                                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                                textStyle: const TextStyle(
+                                    fontSize: 30,
+                                    fontFamily: 'Robot',
+                                    fontWeight: FontWeight.bold)),
+                            child: const Text(''),
                             onPressed: () {
                               setState(() {
+                                hoomans = hoomans + 1;
                                 text = phrases[_random.nextInt(phrases.length)];
                               });
 
@@ -177,25 +227,19 @@ class _MyHomePageState extends State<MyHomePage> {
   bool get supportPause => defaultTargetPlatform != TargetPlatform.android;
   bool get supportResume => defaultTargetPlatform != TargetPlatform.android;
 
-  void speak() {
-    tts.setVolume(volume);
-    tts.setRate(rate);
-    if (languageCode != null) {
-      tts.setLanguage(languageCode!);
-    }
-    tts.setPitch(pitch);
-    tts.speak(text);
+  void speak() async {
   }
-  void sayThis(String thisString ) {
+
+  Future<void> sayThis(String thisString ) async {
+    var speak = Uri.http('localhost:8080', 'say');
+
     print("start animation");
-    tts.setVolume(volume);
-    tts.setRate(rate);
-    if (languageCode != null) {
-      tts.setLanguage(languageCode!);
-    }
-    tts.setPitch(pitch);
-    animate(thisString);
-    tts.speak(thisString);
+    Future.delayed(const Duration(milliseconds: 2400), () {
+      animate(thisString);
+
+    });
+    var x = await http.post(speak, body: thisString);
+    print(x);
     print("stop animation");
   }
 
@@ -234,7 +278,7 @@ class OpenPainter extends CustomPainter {
     var paint1 = Paint()
       ..color = Colors.black
       ..style = PaintingStyle.fill;
-    Rect myRect = Offset(globalWidth/2.9, globalHeight/4.27) & Size(20.0, mouth);
+    Rect myRect = Offset(globalWidth/4.6, globalHeight/3.60) & Size(20.0, mouth);
     canvas.drawOval(myRect, paint1);
 //    canvas.drawCircle(Offset(400, 200), 50, paint1);
   }
